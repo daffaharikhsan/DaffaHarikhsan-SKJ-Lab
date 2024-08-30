86a9df2fcc8cfd8ec92f10196d7313210204275e
daffaharikhsan <dharikhsan@gmail.com>
2024-08-30 09:10:00 +0700

    Update

---
+++ ASSIGNMENT 1/1.6.3/Asembbly to C++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    // Deklarasi variabel
+    int num1 = 5;
+    int num2 = 10;
+    int result = 0;
+
+    // Melakukan perkalian
+    result = num1 * num2;
+
+    // Menampilkan hasil
+    std::cout << "Hasil Perkalian: " << result << std::endl;
+
+    return 0;
+}
